285e41577ce2105d8f094a601e8fca240a350115
LEI <154347188@qq.com>
2023-02-03 17:55:34 +0800

    mybatis基础

--- demo-spring/src/main/java/com/hundsun/demo/spring/SpringDemoApplication.java
+++ demo-spring/src/main/java/com/hundsun/demo/spring/SpringDemoApplication.java
@@ -1,7 +1,6 @@
 package com.hundsun.demo.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
---
+++ demo-spring/src/main/java/com/hundsun/demo/spring/model/pojo/TeacherDO.java
@@ -0,0 +1,23 @@
+package com.hundsun.demo.spring.model.pojo;
+
+import lombok.Data;
+
+/**
+ * @ProductName: Hundsun amust
+ * @ProjectName: study-demo
+ * @Package: com.hundsun.demo.springboot.model
+ * @Description:
+ * @Author: hulei42031
+ * @Date: 2022-08-01 14:14
+ * @UpdateRemark:
+ * @Version: 1.0
+ * <p>
+ * Copyright  2022 Hundsun Technologies Inc. All Rights Reserved
+ */
+@Data
+public class TeacherDO {
+
+    String name;
+
+    Integer age;
+}
