e2060d4974c0b2f088bceced02067b31357c426f
woyaoxuexi123 <154347188@qq.com>
2023-02-04 18:35:57 +0800

    一个简单的二叉树类

--- demo-java/src/main/java/com/hundsun/demo/java/datastructure/tree/SimpleBiTreeNode.java
+++ demo-java/src/main/java/com/hundsun/demo/java/datastructure/tree/SimpleBiTreeNode.java
@@ -5,14 +5,9 @@ package com.hundsun.demo.java.datastructure.tree;
  * @projectName: study-demo
  * @package: com.hundsun.demo.java.tree
  * @className: SimpleBiTreeNode
- * @description:
+ * @description: 简单的二叉树
  * @author: h1123
  * @createDate: 2023/2/3 0:26
- * @updateUser: h1123
- * @updateDate: 2023/2/3 0:26
- * @updateRemark:
- * @version: v1.0
- * @see :
  */
 
 public class SimpleBiTreeNode<T> extends BiTreeNode<T> {
