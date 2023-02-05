e83ef87d62a2b9f154ea15a70a9e88c2a5dedb2d
woyaoxuexi123 <154347188@qq.com>
2023-02-05 22:02:47 +0800

    适配器模式

--- demo-java/src/main/java/com/hundsun/demo/java/pattern/adapter/AdvancedMediaPlayer.java
+++
@@ -1,6 +0,0 @@
-package com.hundsun.demo.java.pattern.adapter;
-
-public interface AdvancedMediaPlayer {
-   public void playVlc(String fileName);
-   public void playMp4(String fileName);
-}
--- demo-java/src/main/java/com/hundsun/demo/java/pattern/adapter/MediaPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.hundsun.demo.java.pattern.adapter;
-
-
-/**
- * 媒体播放器接口
- */
-public interface MediaPlayer {
-   public void play(String audioType, String fileName);
-}
