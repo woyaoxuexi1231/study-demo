9f3d7ab54d2027c4fb964477371d2b5fc0fd4e0e
woyaoxuexi123 <154347188@qq.com>
2023-02-05 18:02:59 +0800

    代理模式和三种生成代理的方法

---
+++ demo-java/src/main/java/com/hundsun/demo/java/pattern/proxy/MySQLService.java
@@ -0,0 +1,22 @@
+package com.hundsun.demo.java.pattern.proxy;
+
+/**
+ * @ProductName: Java
+ * @Package: com.hundsun.demo.java.jdk.proxy
+ * @Description:
+ * @Author: HL
+ * @Date: 2021/10/12 16:13
+ * @Version: 1.0
+ * <p>
+ */
+
+public interface MySQLService {
+
+
+    /**
+     * 一个更新接口
+     *
+     * @param arg 入参
+     */
+    void update(String arg);
+}
