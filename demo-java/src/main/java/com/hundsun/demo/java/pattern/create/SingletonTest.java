428f90582a3e05de4d651afd35bd6345f4e567ff
woyaoxuexi123 <154347188@qq.com>
2023-02-05 19:24:48 +0800

    单例模式

---
+++ demo-java/src/main/java/com/hundsun/demo/java/pattern/create/SingletonTest.java
@@ -0,0 +1,30 @@
+package com.hundsun.demo.java.pattern.create;
+
+import com.hundsun.demo.java.pattern.create.singleton.SingletonCar;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.pattern.create
+ * @className: SingletonTest
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/2/5 19:21
+ */
+
+public class SingletonTest {
+
+    /*
+    单例模式 - 确保一个类只有一个实例, 并提供一个全局访问点来访问这个唯一实例
+
+    Spring中的一个核心设计模式
+     */
+
+    public static void main(String[] args) {
+
+        SingletonCar one = SingletonCar.getInstance();
+        SingletonCar two = SingletonCar.getInstance();
+
+        // is always true
+        System.out.println(one == two);
+    }
+}
---
+++ demo-java/src/main/java/com/hundsun/demo/java/pattern/create/singleton/SingletonCar.java
@@ -0,0 +1,32 @@
+package com.hundsun.demo.java.pattern.create.singleton;
+
+import com.hundsun.demo.java.pattern.create.Car;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.pattern.create.singleton
+ * @className: SingletonCar
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/2/5 19:10
+ */
+
+public class SingletonCar extends Car {
+
+    /**
+     * 使用静态内部类实现单例模式 - IoDH Initialization on Demand Holder
+     * <p>
+     * 静态内部类没有作为 SingletonCar 的成员变量内直接实例化, 因此类加载时不会实例化 SingletonCar
+     * 第一次调用 getInstance时会加载内部类HolderClass, 在内部类定义了一个 static类型的变量 instance
+     * 此时会首先初始化这个成员变量, 由 Java虚拟机来保证其线程安全, 确保成员变量只能初始化一次, 由于没有上锁, 性能没有任何影响
+     * <p>
+     * 除此之外还有饿汉式加载(类加载时就创建唯一实例)、懒汉式+双重检查锁定
+     */
+    private static class HolderClass {
+        private final static SingletonCar instance = new SingletonCar();
+    }
+
+    public static SingletonCar getInstance() {
+        return HolderClass.instance;
+    }
+}
