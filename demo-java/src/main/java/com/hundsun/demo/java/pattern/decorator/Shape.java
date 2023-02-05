23495d68543ff6bff5ab01f82c58b1c6ff5735ae
woyaoxuexi123 <154347188@qq.com>
2023-02-05 22:03:29 +0800

    装饰器模式

--- demo-java/src/main/java/com/hundsun/demo/java/pattern/decorator/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.hundsun.demo.java.pattern.decorator;
-
-public interface Shape {
-   void draw();
-}
---
+++ demo-java/src/main/java/com/hundsun/demo/java/pattern/structural/decorator/AdapterCutter.java
@@ -0,0 +1,38 @@
+package com.hundsun.demo.java.pattern.structural.decorator;
+
+import com.hundsun.demo.java.pattern.structural.Adapter;
+import com.hundsun.demo.java.pattern.structural.Power;
+import com.hundsun.demo.java.pattern.structural.Power20;
+import com.hundsun.demo.java.pattern.structural.Power5;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.pattern.structural.decorator
+ * @className: AdapterCutter
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/2/5 21:21
+ */
+
+public class AdapterCutter extends AdapterDecorator {
+
+    Power power;
+
+    public AdapterCutter(Adapter adapter) {
+
+        // 降压操作
+        int voltage = adapter.getPower().getVoltage();
+        if (voltage == 5) {
+            this.power = adapter.getPower();
+        } else if (voltage == 20) {
+            this.power = new Power5(adapter.getPower().getElectron());
+        } else {
+            this.power = new Power20(adapter.getPower().getElectron());
+        }
+    }
+
+    @Override
+    public Power getPower() {
+        return power;
+    }
+}
