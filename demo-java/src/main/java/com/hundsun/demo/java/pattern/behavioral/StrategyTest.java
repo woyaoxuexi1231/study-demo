ee9d33f13b1869fd1e8275b3451604ad42c00801
woyaoxuexi123 <154347188@qq.com>
2023-02-05 22:54:37 +0800

    行为型模式

---
+++ demo-java/src/main/java/com/hundsun/demo/java/pattern/behavioral/StrategyTest.java
@@ -0,0 +1,17 @@
+package com.hundsun.demo.java.pattern.behavioral;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.pattern.behavioral
+ * @className: StrategyTest
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/2/5 22:42
+ */
+
+public class StrategyTest {
+
+    /*
+    策略模式 - 定义一系列算法, 将每个算法封装起来, 并让它们可以相互替换.
+     */
+}
--- demo-java/src/main/java/com/hundsun/demo/java/pattern/command/Order.java
+++
@@ -1,5 +0,0 @@
-package com.hundsun.demo.java.pattern.command;
-
-public interface Order {
-   void execute();
-}
