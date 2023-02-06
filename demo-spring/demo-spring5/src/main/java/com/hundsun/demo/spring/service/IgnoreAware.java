3b396e437f4ef292c9660dd9b99fd4b03987fc19
woyaoxuexi123 <154347188@qq.com>
2023-02-07 01:04:35 +0800

    BeanFactoryPostProcessor

--- demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/service/IgnoreAware.java
+++ demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/service/IgnoreAware.java
@@ -1,7 +1,5 @@
 package com.hundsun.demo.spring.service;
 
-import com.hundsun.demo.spring.model.IgnoreBean;
-
 /**
  * @projectName: study-demo
  * @package: com.hundsun.demo.spring.service
@@ -9,14 +7,8 @@ import com.hundsun.demo.spring.model.IgnoreBean;
  * @description:
  * @author: h1123
  * @createDate: 2022/11/17 20:56
- * @updateUser: h1123
- * @updateDate: 2022/11/17 20:56
- * @updateRemark:
- * @version: v1.0
- * @see :
  */
 
 public interface IgnoreAware {
 
-    public void setIgnoreBean(IgnoreBean ignoreBean);
 }
---
+++ demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/service/impl/IgnoreBean.java
@@ -0,0 +1,15 @@
+package com.hundsun.demo.spring.service.impl;
+
+import com.hundsun.demo.spring.service.IgnoreAware;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.spring.model
+ * @className: IgnoreBean
+ * @description:
+ * @author: h1123
+ * @createDate: 2022/11/17 20:57
+ */
+public class IgnoreBean implements IgnoreAware {
+
+}
