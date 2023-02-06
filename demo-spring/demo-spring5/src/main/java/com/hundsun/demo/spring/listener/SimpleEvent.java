64d175dde2d88880dafa61187c70e3b126012aa4
woyaoxuexi123 <154347188@qq.com>
2023-02-07 01:02:23 +0800

    一个简单的监听器

--- demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/listener/SimpleEvent.java
+++ demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/listener/SimpleEvent.java
@@ -1,4 +1,4 @@
-package com.hundsun.demo.spring.event;
+package com.hundsun.demo.spring.listener;
 
 import org.springframework.context.ApplicationEvent;
 
@@ -6,19 +6,14 @@ import org.springframework.context.ApplicationEvent;
  * @projectName: study-demo
  * @package: com.hundsun.demo.spring.event
  * @className: SimpleEvent
- * @description:
+ * @description: 一个简单的事件
  * @author: h1123
  * @createDate: 2022/11/20 19:26
- * @updateUser: h1123
- * @updateDate: 2022/11/20 19:26
- * @updateRemark:
- * @version: v1.0
- * @see :
  */
 
 public class SimpleEvent extends ApplicationEvent {
 
-    public SimpleEvent(Object source) {
-        super(source);
+    public SimpleEvent(String eventName) {
+        super(eventName);
     }
 }
--- demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/listener/SimpleListener.java
+++ demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/listener/SimpleListener.java
@@ -1,6 +1,6 @@
 package com.hundsun.demo.spring.listener;
 
-import com.hundsun.demo.spring.event.SimpleEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 
 /**
@@ -10,17 +10,20 @@ import org.springframework.context.ApplicationListener;
  * @description:
  * @author: h1123
  * @createDate: 2022/11/20 19:27
- * @updateUser: h1123
- * @updateDate: 2022/11/20 19:27
- * @updateRemark:
- * @version: v1.0
- * @see :
  */
 
+@Slf4j
 public class SimpleListener implements ApplicationListener<SimpleEvent> {
 
+    /*
+    ApplicationContext事件机制是观察者设计模式的实现, 通过 ApplicationEvent 类和 ApplicationListener 接口, 可以实现 ApplicationContext 事件处理
+    如果容器中有一个 ApplicationListener Bean, 每当 ApplicationContext 发布 ApplicationEvent 时, ApplicationListener Bean将自动被触发, 这种事件机制都必须需要程序显示的触发
+    其中spring有一些内置的事件, 当完成某种操作时会发出某些事件动作, 比如监听ContextRefreshedEvent事件, 当所有的bean都初始化完成并被成功装载后会触发该事件, 实现ApplicationListener<ContextRefreshedEvent>接口可以收到监听动作, 然后可以写自己的逻辑
+
+    这里实现一个简单的事件监听
+     */
     @Override
-    public void onApplicationEvent(SimpleEvent simpleEvent) {
-        System.out.println("事件进来咯");
+    public void onApplicationEvent(SimpleEvent event) {
+        log.info("A message is received: {}", event.getSource());
     }
 }
