6f1bf33d0ecd6563f89d6e24f4034342c13abf85
woyaoxuexi123 <154347188@qq.com>
2023-02-26 22:35:22 +0800

    MQ配置

---
+++ demo-java/src/main/java/com/hundsun/demo/java/mq/MQConfig.java
@@ -0,0 +1,19 @@
+package com.hundsun.demo.java.mq;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.mq
+ * @className: MQConfig
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/2/26 19:00
+ */
+
+public class MQConfig {
+
+    /* RabbitMQ */
+    public static final String RABBITMQ_USERNAME = "rabbitmq";
+    public static final String RABBITMQ_PASSWORD = "rabbitmq";
+    public static final String RABBITMQ_HOST = "192.168.175.128";
+    public static final Integer RABBITMQ_PORT = 5672;
+}
