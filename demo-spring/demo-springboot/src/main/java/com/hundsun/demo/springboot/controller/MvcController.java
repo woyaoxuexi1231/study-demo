4b0f44a10441e0aa2a92c5309016cd541d93cfa2
woyaoxuexi123 <154347188@qq.com>
2023-04-04 00:27:29 +0800

    引入bootstrap

--- demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/controller/MvcController.java
+++ demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/controller/MvcController.java
@@ -1,8 +1,12 @@
 package com.hundsun.demo.springboot.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 /**
  * @projectName: study-demo
@@ -17,8 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class MvcController {
 
-    @RequestMapping("/index")
-    public String test(){
-        return "index";
+    @Autowired
+    TemplateEngine engine;
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String test() {
+        Context context = new Context();
+        context.setVariable("name", "name");
+        return engine.process("index", context);
     }
 }
