44a34f6d47bc475488130d4f879a12bab69eafd1
woyaoxuexi123 <154347188@qq.com>
2023-05-07 15:38:27 +0800

    springcloud - 使用 turbine 聚合监控

---
+++ demo-spring/demo-springcloud/eureka-monitor-client/src/main/java/com/hundsun/demo/springcloud/eureka/monitor/EurekaMonitorApplication.java
@@ -0,0 +1,33 @@
+package com.hundsun.demo.springcloud.eureka.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.springcloud.eureka.server
+ * @className: EurekaServerApplication
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/5/5 20:44
+ */
+
+@EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaMonitorApplication {
+
+    public static void main(String[] args) {
+
+        /*
+        DiscoveryClient_EUREKA-CLIENT/LAPTOP-HGITO649:eureka-client:9101 - registration status: 204
+        注册成功
+         */
+        SpringApplication.run(EurekaMonitorApplication.class, args);
+    }
+}
