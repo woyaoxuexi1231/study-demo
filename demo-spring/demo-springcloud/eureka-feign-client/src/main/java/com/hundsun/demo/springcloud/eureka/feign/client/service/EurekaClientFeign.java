690c9495916e29153e4edf456b6124a7a0d7f4e8
woyaoxuexi123 <154347188@qq.com>
2023-05-07 14:47:29 +0800

    springcloud - eureka-feign-client 引入熔断器(hystrix)

--- demo-spring/demo-springcloud/eureka-feign-client/src/main/java/com/hundsun/demo/springcloud/eureka/feign/client/service/EurekaClientFeign.java
+++ demo-spring/demo-springcloud/eureka-feign-client/src/main/java/com/hundsun/demo/springcloud/eureka/feign/client/service/EurekaClientFeign.java
@@ -1,6 +1,7 @@
 package com.hundsun.demo.springcloud.eureka.feign.client.service;
 
 import com.hundsun.demo.springcloud.eureka.feign.client.config.FeignConfig;
+import com.hundsun.demo.springcloud.eureka.feign.client.service.impl.HiHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @createDate: 2023/5/7 13:17
  */
 
-@FeignClient(value = "eureka-client", configuration = FeignConfig.class)
+@FeignClient(value = "eureka-client", configuration = FeignConfig.class, fallback = HiHystrix.class)
 public interface EurekaClientFeign {
 
     @GetMapping("/hi")
