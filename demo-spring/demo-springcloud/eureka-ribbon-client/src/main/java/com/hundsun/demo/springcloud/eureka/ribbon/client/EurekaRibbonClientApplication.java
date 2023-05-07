2f9b4ce0a67307ab8333f38b9aa8798cdfd27558
woyaoxuexi123 <154347188@qq.com>
2023-05-07 15:09:51 +0800

    springcloud - eureka-ribbon-client 使用 HystrixDashboard监控熔断器状态

--- demo-spring/demo-springcloud/eureka-ribbon-client/src/main/java/com/hundsun/demo/springcloud/eureka/ribbon/client/EurekaRibbonClientApplication.java
+++ demo-spring/demo-springcloud/eureka-ribbon-client/src/main/java/com/hundsun/demo/springcloud/eureka/ribbon/client/EurekaRibbonClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @projectName: study-demo
@@ -14,6 +15,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
  * @createDate: 2023/5/5 20:44
  */
 
+@EnableHystrixDashboard
 @EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
