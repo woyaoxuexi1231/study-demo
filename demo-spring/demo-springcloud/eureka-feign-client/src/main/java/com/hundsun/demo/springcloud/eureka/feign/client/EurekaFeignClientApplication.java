8f2b6ea58fcf40bcf2a0f200db9937cc6e9048c3
woyaoxuexi123 <154347188@qq.com>
2023-05-07 15:16:34 +0800

    springcloud - eureka-feign-client 使用 HystrixDashboard监控熔断器状态

--- demo-spring/demo-springcloud/eureka-feign-client/src/main/java/com/hundsun/demo/springcloud/eureka/feign/client/EurekaFeignClientApplication.java
+++ demo-spring/demo-springcloud/eureka-feign-client/src/main/java/com/hundsun/demo/springcloud/eureka/feign/client/EurekaFeignClientApplication.java
@@ -3,6 +3,8 @@ package com.hundsun.demo.springcloud.eureka.feign.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -14,6 +16,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @createDate: 2023/5/5 20:44
  */
 
+@EnableHystrixDashboard
+@EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
