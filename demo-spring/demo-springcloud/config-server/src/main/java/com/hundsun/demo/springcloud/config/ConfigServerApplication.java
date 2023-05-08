322d1472018ddebeb20ed089ca31f458273806bf
LEI <154347188@qq.com>
2023-05-08 20:14:54 +0800

    springcloud - config-server

---
+++ demo-spring/demo-springcloud/config-server/src/main/java/com/hundsun/demo/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,28 @@
+package com.hundsun.demo.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.springcloud.eureka.server
+ * @className: EurekaServerApplication
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/5/5 20:44
+ */
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+
+        /*
+        DiscoveryClient_EUREKA-CLIENT/LAPTOP-HGITO649:eureka-client:9101 - registration status: 204
+        注册成功
+         */
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
