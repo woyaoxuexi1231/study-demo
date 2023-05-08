9a0b25b120383283dd4e7f5f18b9c760faaf6cf4
woyaoxuexi123 <154347188@qq.com>
2023-05-09 00:44:21 +0800

    springcloud - security

---
+++ demo-spring/demo-springcloud/security/src/main/java/com/hundsun/demo/springcloud/security/SecurityApplication.java
@@ -0,0 +1,21 @@
+package com.hundsun.demo.springcloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.springcloud.eureka.server
+ * @className: EurekaServerApplication
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/5/5 20:44
+ */
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
---
+++ demo-spring/demo-springcloud/security/src/main/java/com/hundsun/demo/springcloud/security/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.hundsun.demo.springcloud.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.springcloud.security.config
+ * @className: SecurityConfig
+ * @description: 配置如何验证用户信息
+ * @author: h1123
+ * @createDate: 2023/5/9 0:05
+ */
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 通过 AuthenticationManagerBuilder 创建一个认证用户的信息
+     * <p>
+     * 用户名为 admin, 密码为 admin, USER 的角色
+     *
+     * @param authenticationManagerBuilder
+     * @throws Exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("admin"))
+                .roles("USER");
+    }
+
+    /**
+     * 配置 HttpSecurity
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/blogs/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error")
+                .and()
+                .exceptionHandling().accessDeniedPage("/401");
+        http.logout().logoutSuccessUrl("/");
+    }
+}
