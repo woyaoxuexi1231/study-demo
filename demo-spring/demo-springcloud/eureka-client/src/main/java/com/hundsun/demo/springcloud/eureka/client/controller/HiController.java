f8fcdd23fc5bae20dcb7c14a7cdb102a5eb6df67
woyaoxuexi123 <154347188@qq.com>
2023-05-15 22:19:39 +0800

    RestTemplate - GET请求的多种接收参数的方式

--- demo-spring/demo-springcloud/eureka-client/src/main/java/com/hundsun/demo/springcloud/eureka/client/controller/HiController.java
+++ demo-spring/demo-springcloud/eureka-client/src/main/java/com/hundsun/demo/springcloud/eureka/client/controller/HiController.java
@@ -1,7 +1,12 @@
 package com.hundsun.demo.springcloud.eureka.client.controller;
 
+import com.hundsun.demo.commom.core.model.dto.ResultDTO;
+import com.hundsun.demo.commom.core.model.req.SimpleReqDTO;
+import com.hundsun.demo.commom.core.utils.ResultDTOBuild;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,5 +30,41 @@ public class HiController {
     @GetMapping("/hi")
     public String hi(HttpServletRequest req, HttpServletResponse rsp) {
         return "here is " + port + " and your ip is " + req.getRemoteAddr();
+    }
+
+    /**
+     * 普通的 URL 格式, 参数通过 hi2?req=xxx 带入请求
+     * 参数名必须一致
+     *
+     * @param req
+     * @return
+     */
+    @GetMapping("/hi2")
+    public ResultDTO<?> hi2(@RequestParam(required = false, name = "req") String req) {
+        return ResultDTOBuild.resultSuccessBuild("OK, im receive the msg : " + req);
+    }
+
+    /**
+     * 请求中带有占位符的 URL
+     * 注解 @PathVariable 绑定参数中的占位符
+     * 参数一个不能少
+     *
+     * @param req 与占位符名字对应的参数
+     * @return result
+     */
+    @GetMapping(path = {"/hi3/{req}/{other}"}, name = "Restful 风格的 URL 请求")
+    public ResultDTO<?> hi3(@PathVariable(name = "req") String req, @PathVariable(name = "other") String other) {
+        return ResultDTOBuild.resultSuccessBuild("OK, im receive the msg : " + req + ", " + other);
+    }
+
+    /**
+     * 当参数过多, 考虑用对象来接收请求
+     *
+     * @param req 对象参数
+     * @return result
+     */
+    @GetMapping("/hi4")
+    public ResultDTO<?> hi4(SimpleReqDTO req) {
+        return ResultDTOBuild.resultSuccessBuild("OK, im receive the msg : " + req.toString());
     }
 }
--- demo-spring/demo-springcloud/eureka-ribbon-client/src/main/java/com/hundsun/demo/springcloud/eureka/ribbon/client/service/RibbonService.java
+++ demo-spring/demo-springcloud/eureka-ribbon-client/src/main/java/com/hundsun/demo/springcloud/eureka/ribbon/client/service/RibbonService.java
@@ -1,9 +1,16 @@
 package com.hundsun.demo.springcloud.eureka.ribbon.client.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.hundsun.demo.commom.core.model.dto.ResultDTO;
+import com.hundsun.demo.commom.core.model.req.SimpleReqDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @projectName: study-demo
@@ -14,15 +21,68 @@ import org.springframework.web.client.RestTemplate;
  * @createDate: 2023/5/6 23:04
  */
 
+@Slf4j
 @Service
 public class RibbonService {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "hiError")
+    // @HystrixCommand(fallbackMethod = "hiError")
     public String hi() {
-        return restTemplate.getForObject("http://eureka-client/hi", String.class);
+
+        String url = "http://eureka-client/";
+        StringBuffer sb = new StringBuffer();
+
+        // 无参格式
+        sb.append(restTemplate.getForObject(url + "hi", String.class)).append("\n");
+
+        sb.append(restTemplate.getForObject(url + "hi2?req={1}", ResultDTO.class, "hello")).append("\n");
+
+        Map<String, String> reqMap = new HashMap<>();
+        reqMap.put("req", "hello");
+        reqMap.put("other", "ps");
+        reqMap.put("reqString", "hello");
+        sb.append(restTemplate.getForObject(url + "hi3/{req}/{other}", ResultDTO.class, reqMap)).append("\n");
+
+        SimpleReqDTO req = new SimpleReqDTO();
+        req.setReqString("hello");
+
+        /*
+        To pass an object in restTemplate get method, you can use the UriComponentsBuilder class to build the URL with query parameters. Here is an example:
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = UriComponentsBuilder.fromUriString("http://localhost:8080/api/endpoint")
+            .queryParam("param1", "value1")
+            .queryParam("param2", "value2")
+            .build()
+            .toUri();
+        MyObject myObject = restTemplate.getForObject(uri, MyObject.class);
+
+        You can also use the HttpEntity class to pass the object as a request parameter. Here is an example:
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        MyObject myObject = new MyObject("value1", "value2");
+        HttpEntity<MyObject> entity = new HttpEntity<>(myObject, headers);
+        MyObject response = restTemplate.exchange(
+            "http://localhost:8080/api/endpoint?param1={param1}&param2={param2}",
+            HttpMethod.GET,
+            entity,
+            MyObject.class,
+            myObject.getParam1(),
+            myObject.getParam2()
+        ).getBody();
+         */
+        // 这里搞了很久, 不知道怎么才能传递对象, 这个用 chatGPT 搜出来的结果, 点个赞!
+        URI uri = UriComponentsBuilder.fromUriString(url + "hi4")
+                .queryParam("reqString", "hello")
+                .build()
+                .toUri();
+        sb.append(restTemplate.getForObject(uri, ResultDTO.class)).append("\n");
+
+        sb.append(restTemplate.getForEntity(url + "hi2?req={1}", ResultDTO.class, "hello")).append("\n");
+
+        return sb.toString();
     }
 
     public String hiError() {
