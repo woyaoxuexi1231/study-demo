ceb95fa1332b81ff6e404aae427fce1c5963a4c4
LEI <154347188@qq.com>
2023-06-14 19:42:15 +0800

    UTF8 转 GBK

---
+++ demo-java/src/main/java/com/hundsun/demo/java/util/UTF82GBK.java
@@ -0,0 +1,29 @@
+package com.hundsun.demo.java.util;
+
+import java.nio.charset.Charset;
+
+
+public class UTF82GBK {
+
+    public static void main(String[] args) throws Exception {
+        //系统的默认编码是GBK
+        System.out.println("Default Charset=" + Charset.defaultCharset());
+        String t = "hfjkds中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国中国hfsdkj<img src='sasa' /> fjldsajflkdsjaflkdsjalf <img src='sada' ait=''/>sfdsfadas";  
+        //思路：先转为Unicode，然后转为GBK
+        String utf8 = new String(t.getBytes( "UTF-8"));
+        //等同于:
+//        String utf8 = new String(t.getBytes( "UTF-8"),Charset.defaultCharset());
+        
+        System.out.println(utf8);  
+        String unicode = new String(utf8.getBytes(),"UTF-8");   
+        //等同于:
+//        String unicode = new String(utf8.getBytes(Charset.defaultCharset()),"UTF-8");   
+        System.out.println(unicode);  
+        String gbk = new String(unicode.getBytes("GBK"));  
+        //等同于:
+//        String gbk = new String(unicode.getBytes("GBK"),Charset.defaultCharset());  
+          
+        System.out.println(gbk);  
+    }
+
+}
