8b36aac89bb16b8cda7b387b6817a46d60d917e6
woyaoxuexi123 <154347188@qq.com>
2023-07-26 23:01:41 +0800

    网络IO模型学习

---
+++ demo-java/src/main/java/com/hundsun/demo/java/jdk/network/IONote.java
@@ -0,0 +1,17 @@
+package com.hundsun.demo.java.jdk.network;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.jdk.network
+ * @className: IONote
+ * @description:
+ * @author: hl
+ * @createDate: 2023/7/26 22:53
+ */
+
+public class IONote {
+    /*
+     * 1. todo-2023-07-26 为什么两台计算机通信一定要用 socket?
+     * 2. todo-2023-07-26 五种主要的 IO 模型
+     * */
+}
