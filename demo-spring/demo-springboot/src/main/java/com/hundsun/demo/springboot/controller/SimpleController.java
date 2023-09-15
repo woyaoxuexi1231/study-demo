c99d1132fd2d46c4e24453f48bb85153982dcad3
woyaoxuexi123 <154347188@qq.com>
2023-09-16 01:20:08 +0800

    线程池的小笔记

--- demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/controller/SimpleController.java
+++ demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/controller/SimpleController.java
@@ -32,7 +32,12 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.text.NumberFormat;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -217,5 +222,30 @@ public class SimpleController implements ApplicationContextAware {
             }
         }
         return sb.toString();
+    }
+
+    @GetMapping("/threadPool")
+    public void threadPool() {
+        log.info("threadPool");
+        /*
+         * 阿里云强制不允许这种创建方式
+         * 1. FixedThreadPool 和 SingleThreadPool: 允许的请求队列长度为 Integer.MAX_VALUE, 可能会堆积大量的请求, 从而导致 OOM。
+         * 2. CachedThreadPool 和 ScheduledThreadPool: 允许的创建线程数量为Integer.MAX_VALUE, 可能会创建大量的线程, 从而导致OOM。
+         */
+        // ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1);
+        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1, r -> {
+            Thread thread = new Thread(r);
+            thread.setName("scheduled-thread");
+            thread.setDaemon(true);
+            return thread;
+        });
+        scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            log.info("定时任务执行.");
+            throw new RuntimeException("error");
+        }, 1L, 1L, TimeUnit.SECONDS);
+
+        ExecutorService pool = new ThreadPoolExecutor(2, 4, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(2)) {
+
+        };
     }
 }
