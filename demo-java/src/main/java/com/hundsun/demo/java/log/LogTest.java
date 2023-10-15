c23bc231a5b7d7d8806cb41b2636dcc92d9262f4
lei <154347188@qq.com>
2023-10-16 00:46:53 +0800

    日记框架学习笔记

--- demo-java/src/main/java/com/hundsun/demo/java/log/LogTest.java
+++ demo-java/src/main/java/com/hundsun/demo/java/log/LogTest.java
@@ -12,18 +12,30 @@ package com.hundsun.demo.java.log;
 public class LogTest {
 
     /*
-    日志框架的进化史 - log4j[Apache] -> JUL[sun] -> JCL(commons logging) -> slf4j&logback -> log4j2[Apache]
+    日志框架的进化史 - log4j[Apache](1999) -> JUL[sun](2002) -> JCL(commons logging)(2002) -> slf4j&logback(2005,2006) -> log4j2[Apache](2014)
+
+    log4j 最早的日志框架, 是 apache 基金会一个项目, 由 Ceki Gülcü 创建.
+    -> 随后apache想让sun公司把log4j并入jdk遭到了拒绝, sun又模仿log4j推出了JUL(java.util.logging)
+    -> 为了解耦日志的接口与实现, apache紧接着推出了 JCL(Jakarta Commons Logging), 当时具体的实现由 log4j和JUL 完成
+    -> Ceki Gülcü 后来与 Apache 基金会关于 Commons-Logging 制定的标准存在分歧, Ceki Gülcü 离开 Apache 并先后创建了 Slf4j(门面,同样支持log4j和JUL) 和 Logback(实现) 两个项目
+    -> 为了维护在 Java 日志江湖的地位, 防止 JCL、Log4j 被 Slf4j、Logback 组合取代, 2014 年 Apache 推出了 Log4j2, Log4j2 与 log4j 不兼容, 经过大量深度优化, 其性能显著提升
 
     日志框架分为日志门面(只提供相应API而不提供接口实现)和日志系统(提供具体的接口实现)
-    日志门面: commons-logging, slf4j
-    日志系统: log4j, JUL, Logback, slf4j-simple
+    日志门面: commons-logging(JCL), slf4j
+    日志系统: log4j(log4j2), JUL, Logback, slf4j-simple
 
-    日志库适配器: jul-to-slf4j, log4j-over-slf4j, jcl-over-slf4j
-    日志门面适配器: slf4j-jdk, slf4j-jcl, slf4j-log4j12
+     Ceki Gülcü 当年因为觉得 Commons-Logging 的 API 设计的不好, 性能也不够高因而设计了 Slf4j。
+     而他为了 Slf4j 能够兼容各种类型的日志系统实现，还设计了相当多的 adapter 和 bridge 来连接, 如下:
+    日志库适配器: 这几个桥接包都是为了让其他的日志框架都统一输出到 slf4j这个日志框架上
+        jul-to-slf4j,
+        log4j-over-slf4j - 它的主要作用是将使用log4j日志框架输出的日志路由到SLF4J上
+        jcl-over-slf4j
+    日志门面适配器: 主要作用是将slf4j的API和 xxx链接起来, 这样当你在代码中使用SLF4J API进行日志记录时，这些日志实际上会被路由到log4j进行处理, 这种设计使得我们可以很容易地切换具体的日志框架，比如从logback切换到log4j
+        slf4j-jdk, slf4j-jcl, slf4j-log4j12(旧版本的log4j(即log4j 1.x)连接起来的桥接器)
 
     常用的日志搭配:
-        slf4j+logback 不需要桥接包
-        slf4j+log4j2 需要桥接包 log4j-slf4j-impl
+        slf4j+logback 不需要桥接包(都是 Ceki Gülcü 写的一套东西, 他自己的东西肯定不需要适配的)
+        slf4j+log4j2 需要桥接包 log4j-slf4j-impl(和log4j2连接起来的桥接器)
         commons logging+log4j2 不需要桥接包
      */
 }
