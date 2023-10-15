ff8f222167b05d668dc21d9e3106cb008ec8eb1e
lei <154347188@qq.com>
2023-10-15 19:34:01 +0800

    mybatis不整合任何其他框架的简单demo

---
+++ demo-java/src/main/java/com/hundsun/demo/java/db/mybatis/MybatisTest.java
@@ -0,0 +1,40 @@
+package com.hundsun.demo.java.db.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.java.db.mybatis
+ * @className: MybatisTest
+ * @description:
+ * @author: h1123
+ * @createDate: 2023/10/15 19:14
+ */
+
+public class MybatisTest {
+
+    public static void main(String[] args) throws IOException {
+        // 读取配置文件
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        // 创建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        // 创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            // 获取Mapper
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            // 调用Mapper方法
+            User user = userMapper.getUser(1);
+            System.out.println(user.getName());
+        } finally {
+            sqlSession.close();
+
+        }
+    }
+}
