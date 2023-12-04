7f336722776a988c5c41a86804424622516a01c5
LEI <154347188@qq.com>
2023-12-04 21:20:07 +0800

    关于声明一个方法时, 关键字的先后顺序的小笔记

--- demo-java/src/main/java/com/hundsun/demo/java/jdk/generic/GenericMain.java
+++ demo-java/src/main/java/com/hundsun/demo/java/jdk/generic/GenericMain.java
@@ -31,4 +31,10 @@ public class GenericMain {
             System.out.println("hello");
         }
     }
+
+    public static <T> void hhh(T t) {
+        // nothing
+        // 按照惯例，在方法声明中，先写访问修饰符（如 public），然后写其他修饰符（如 static），再写泛型声明（如 <T>），最后写返回类型（如 void）。
+        // 当然，这只是约定俗成的写法，并不是强制要求。- 说是约定成俗的规矩 其实不是, 调换泛型声明的顺序编译器就不再识别了, 我觉得 gpt 回答得有问题
+    }
 }
