3ed96a2ed69335bb6f6a03ef8220f47ddfdc78e1
LEI <154347188@qq.com>
2023-12-21 15:37:52 +0800

    maven3.8.1不再支持http仓库解决

---
+++ demo-java/src/main/java/com/hundsun/demo/java/MavenNote.java
@@ -0,0 +1,29 @@
+package com.hundsun.demo.java;
+
+/**
+ * @ProductName: Hundsun amust
+ * @ProjectName: study-demo
+ * @Package: com.hundsun.demo.java
+ * @Description:
+ * @Author: hulei42031
+ * @Date: 2023-12-21 15:36
+ * @UpdateRemark:
+ * @Version: 1.0
+ * <p>
+ * Copyright 2023 Hundsun Technologies Inc. All Rights Reserved
+ */
+
+public class MavenNote {
+    /*
+    Since Maven 3.8.1 http repositories are blocked.
+    把这个版本的maven全局配置里的配置改一下, 把配置里的这个给注释掉就行了
+    <mirror>
+      <id>maven-default-http-blocker</id>
+      <mirrorOf>external:http:*</mirrorOf>
+      <name>Pseudo repository to mirror external repositories initially using HTTP.</name>
+      <url>http://0.0.0.0/</url>
+      <blocked>true</blocked>
+    </mirror>
+    -->
+     */
+}
