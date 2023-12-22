533e8487a2344907e38e6a840a117dc446f708d2
LEI <154347188@qq.com>
2023-12-22 22:16:04 +0800

    rabbitmq客户端开发基础④ 新增一个ReturnListener

--- demo-java/src/main/java/com/hundsun/demo/java/mq/rabbit/work/MsgProducer.java
+++ demo-java/src/main/java/com/hundsun/demo/java/mq/rabbit/work/MsgProducer.java
@@ -52,6 +52,28 @@ public class MsgProducer {
                 channel.confirmSelect();
                 // 添加一个成功回调和一个失败的回调
                 channel.addConfirmListener(new MsgConfirmSuccessCallBack(), new MsgConfirmFailedCallBack());
+                channel.addReturnListener(
+                        /*
+                        用于处理当消息无法被路由到对应的队列时触发的返回（Return）事件。
+                        方法参数的含义如下：
+
+                        replyCode: 返回的响应码，用于指示返回事件的类型。一般情况下，非零的值表示消息无法被路由到目标队列。
+                        replyText: 返回的响应文本，描述了返回事件的具体信息。
+                        exchange: 发送消息时指定的交换机名称。
+                        routingKey: 发送消息时指定的路由键。
+                        properties: 消息的属性，如消息类型、优先级等。
+                        body: 消息的内容，以字节数组形式表示。
+                        当消息被发送到交换机时，如果交换机无法将消息路由到任何队列中，就会触发返回事件。这通常是因为交换机没有匹配的绑定规则，或者路由键与绑定规则不匹配。
+
+                        通过实现 handleReturn 方法，并在 Channel 中设置 addReturnListener，可以在发送消息后处理返回事件。
+                        当返回事件发生时，RabbitMQ 会调用 handleReturn 方法，并将相关的信息作为参数传递给该方法，以供应用程序进行处理。
+
+                        应用程序可以根据返回事件的信息进行适当的处理，比如记录日志、重新发送消息、或者执行其他特定的逻辑。
+                        总之，handleReturn 方法用于处理 RabbitMQ 返回（Return）事件，当消息无法被路由到目标队列时，该方法会被调用，开发人员可以在这个方法中编写逻辑来处理返回事件。
+                         */
+                        (replyCode, replyText, exchange, routingKey1, properties, body) -> {
+
+                        });
             }
             /**
              * BasicProperties属性笔记
@@ -90,6 +112,7 @@ public class MsgProducer {
              * String exchange, 交换机
              * String routingKey, 路由键
              * BasicProperties props, 消息属性（需要单独声明）
+             * mandatory true:交换机无法根据自身的类型和路由键找到一个符合条件的队列,那么会调用Basic.return命令将消息返回给生产者 false:出现上述情况,消息将直接被丢弃
              * byte[] body，消息体
              */
             channel.basicPublish(exchangeName, routingKey, basicProperties, msg.getBytes());
