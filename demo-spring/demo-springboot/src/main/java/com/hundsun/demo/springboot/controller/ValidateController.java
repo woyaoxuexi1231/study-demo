70dc6d2b71da31b2111a45d4cb95c4d1901923cd
LEI <154347188@qq.com>
2024-01-06 17:52:30 +0800

    javax.validation笔记

---
+++ demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/controller/ValidateController.java
@@ -0,0 +1,60 @@
+package com.hundsun.demo.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+/**
+ * @ProductName: Hundsun amust
+ * @ProjectName: study-demo
+ * @Package: com.hundsun.demo.springboot.controller
+ * @Description:
+ * @Author: hulei42031
+ * @Date: 2024-01-05 10:58
+ * @UpdateRemark:
+ * @Version: 1.0
+ * <p>
+ * Copyright 2023 Hundsun Technologies Inc. All Rights Reserved
+ */
+
+@Slf4j
+@RestController
+public class ValidateController {
+
+    @GetMapping("/simpleValidate")
+    public void simpleValidate(User user) {
+        this.createUser(user);
+    }
+
+    public void createUser(User user) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        if (violations.isEmpty()) {
+            log.info("数据合法，执行相应的逻辑");
+        } else {
+            log.error("数据不合法，进行相应的处理");
+        }
+    }
+
+    // 其他方法和代码
+
+    public static class User {
+        @NotEmpty
+        private String name;
+
+        @Email
+        private String email;
+
+        // 其他属性和方法
+    }
+}
