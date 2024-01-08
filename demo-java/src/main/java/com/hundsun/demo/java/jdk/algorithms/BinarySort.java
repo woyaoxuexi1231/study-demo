b0b35ee74225826f99b00ac28ae41b9a631c6b19
LEI <154347188@qq.com>
2024-01-08 21:36:14 +0800

    快速排序

--- demo-java/src/main/java/com/hundsun/demo/java/jdk/algorithms/BinarySort.java
+++ demo-java/src/main/java/com/hundsun/demo/java/jdk/algorithms/BinarySort.java
@@ -1,6 +1,7 @@
 package com.hundsun.demo.java.jdk.algorithms;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @ProductName: Hundsun amust
@@ -18,31 +19,60 @@ import java.util.Arrays;
 public class BinarySort {
 
     public static void main(String[] args) {
-        int[] ints = new int[]{188, 872591, 9534336, 362, 453873};
-        System.out.println(Arrays.toString(doBinarySort(ints)));
+        int[] ints = new int[]{362, 872591, 9534336, 181, 453873};
+        new BinarySort().randomizedQuicksort(ints, 0, ints.length - 1);
+        System.out.println(Arrays.toString(ints));
         System.out.println(Arrays.toString(Arrays.copyOfRange(ints, 1, 2)));
     }
 
-    public static int[] doBinarySort(int[] ints) {
-        // 基线条件
-        if (ints.length <= 1) {
-            return ints;
+    public void randomizedQuicksort(int[] nums, int l, int r) {
+        if (l < r) {
+            // 分割数组, 得到中间指针位置
+            int pos = randomizedPartition(nums, l, r);
+            // 前半部分继续二分
+            randomizedQuicksort(nums, l, pos - 1);
+            // 后半部分继续二分
+            randomizedQuicksort(nums, pos + 1, r);
         }
-        // 设定一个基准值
-        int midIndex = 0;
-        int midValue = ints[0];
-        // 这个基准值将作为整个数组的中间值, 左边所有的值都比他小, 右边所有的值都比他大
-        // 这里采用的策略是, 依次比较这些数字与基准值的大小, 如果比他大, 位置不变, 如果比他小, 那么与基准值调换位置(使用此策略的前提是我以第一个值为基准值)
-        // 结束的条件为完整遍历一次整个数组
-        for (int i = 1; i < ints.length; i++) {
-            if (ints[i] < midValue) {
-                ints[midIndex] = ints[i];
-                ints[i] = midValue;
-                midValue = ints[i];
-                midIndex = i;
+    }
+
+    public int randomizedPartition(int[] nums, int l, int r) {
+        // 1. 随机选一个作为我们的主元素
+        int i = new Random().nextInt(r - l + 1) + l;
+        // 2. 把我们选定的主元素和最后一个元素调换位置, 这个不一定是和最后一个元素换位置, 可以是其他操作
+        swap(nums, r, i);
+        // 3. 分割这个数组
+        return partition(nums, l, r);
+    }
+
+    public int partition(int[] nums, int l, int r) {
+        // 主元素
+        int pivot = nums[r];
+        // 待交换元素的指针
+        int i = l - 1;
+        // 循环将从给定的起始位置开始, 到终止位置结束(不包括主元素)
+        for (int j = l; j <= r - 1; ++j) {
+            // 遇到比主元素小的, 放到整个数组的前方
+            if (nums[j] <= pivot) {
+                /*
+                1. 第一个数字就比主元素小, 那其实这里位置并没有发生变化
+                2. 如果一个元素它没有进入这个判断, 那么代表这个元素比主元素大, 并且它代表的位置是在后续找到比主元素小的元素时进行调换的标记
+                这里的 i 其实是作为下一次替换位置的指针
+                 */
+                i = i + 1;
+                // 交换
+                swap(nums, i, j);
             }
         }
+        // 把主元素和这个待交换元素指针位置的元素交换位置(这个元素必定比主元素大)
+        swap(nums, i + 1, r);
+        // 返回指针位置
+        return i + 1;
+    }
 
-        return ints;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
