993541c3e3d849121a9d420ee3678b5d1d7ea99a
woyaoxuexi <154347188@qq.com>
2024-03-10 18:02:31 +0800

    springboot整合thymeleaf,bootstrap

---
+++ demo-spring/springboot-thymeleaf/src/main/java/com/hundsun/demo/springboot/thymeleaf/model/req/EmployeeQryReqDTO.java
@@ -0,0 +1,17 @@
+package com.hundsun.demo.springboot.thymeleaf.model.req;
+
+import com.hundsun.demo.commom.core.model.req.PageQryReqDTO;
+import lombok.Data;
+
+/**
+ * @projectName: study-demo
+ * @package: com.hundsun.demo.springboot.com.hundsun.demo.springboot.thymeleaf.model.req
+ * @className: EmployeeQryReqDTO
+ * @description:
+ * @author: woaixuexi
+ * @createDate: 2024/3/10 14:23
+ */
+
+@Data
+public class EmployeeQryReqDTO extends PageQryReqDTO {
+}
