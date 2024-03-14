90a3a40cf3598e07f15ccd444578d58a51b713aa
woyaoxuexi <154347188@qq.com>
2024-03-15 01:01:14 +0800

    清除多余代码

--- demo-dubbo/demo-dubbo-provider/demo-dubbo-provider-service/src/main/java/com/hundsun/demo/dubbo/provider/service/impl/SimpleProviderServiceImpl.java
+++ demo-dubbo/demo-dubbo-provider/demo-dubbo-provider-service/src/main/java/com/hundsun/demo/dubbo/provider/service/impl/SimpleProviderServiceImpl.java
@@ -1,17 +1,10 @@
 package com.hundsun.demo.dubbo.provider.service.impl;
 
-import cn.hutool.core.thread.ThreadFactoryBuilder;
 import com.hundsun.demo.commom.core.model.dto.ResultDTO;
 import com.hundsun.demo.commom.core.utils.ResultDTOBuild;
 import com.hundsun.demo.dubbo.provider.api.service.SimpleProviderService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @ProductName: Hundsun amust
@@ -21,23 +14,14 @@ import java.util.concurrent.TimeUnit;
  * @Author: hulei42031
  * @Date: 2022-05-21 15:15
  */
+
 @DubboService
 @Slf4j
 public class SimpleProviderServiceImpl implements SimpleProviderService {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
 
     @Override
     public ResultDTO<?> RpcSimpleInvoke() {
         return ResultDTOBuild.resultSuccessBuild("hello rpc!");
     }
-
-    /**
-     *
-     */
-    private static final ThreadPoolExecutor RABBITMQ_POOL = new ThreadPoolExecutor(5, 20, 20,
-            TimeUnit.SECONDS, new ArrayBlockingQueue<>(150), new ThreadFactoryBuilder()
-            .setNamePrefix("RedissonServiceImpl-thread-").build(), new ThreadPoolExecutor.CallerRunsPolicy());
 
 }
