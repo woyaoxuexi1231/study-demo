eacff6e315be3cfcbd7716e9ddd9f6cc640ff038
woyaoxuexi <154347188@qq.com>
2024-04-09 15:15:43 +0800

    redisson版本修复

--- demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/redis/pub/RedisMessagePublisher.java
+++ demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/redis/pub/RedisMessagePublisher.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping(value = "/redispush")
 @Component
