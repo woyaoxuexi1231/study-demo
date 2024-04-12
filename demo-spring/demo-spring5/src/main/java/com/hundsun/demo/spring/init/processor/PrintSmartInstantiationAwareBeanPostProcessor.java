70516ea238bd8904cc547cdc8540a342978e2267
woyaoxuexi <154347188@qq.com>
2024-04-12 20:24:45 +0800

    笔记整理

--- demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/init/processor/PrintSmartInstantiationAwareBeanPostProcessor.java
+++ demo-spring/demo-spring5/src/main/java/com/hundsun/demo/spring/init/processor/PrintSmartInstantiationAwareBeanPostProcessor.java
@@ -29,6 +29,9 @@ public class PrintSmartInstantiationAwareBeanPostProcessor implements SmartInsta
 
     @Override
     public Object getEarlyBeanReference(Object bean, String beanName) throws BeansException {
+        // 在 AOP 功能中，可以使用 getEarlyBeanReference 方法提前创建代理对象，以便在 Bean 实例化完成后立即应用切面逻辑。
+        // 在需要提前处理 Bean 的某些属性或状态的情况下，可以在此方法中进行相关操作。
+        // 在org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator.getEarlyBeanReference就实现了代理对象的生成
         // log.info("this is PrintSmartInstantiationAwareBeanPostProcessor");
         return SmartInstantiationAwareBeanPostProcessor.super.getEarlyBeanReference(bean, beanName);
     }
