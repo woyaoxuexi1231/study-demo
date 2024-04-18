a4a4fc607fdc24f0ff5ea9b709e87fb4a955ceb3
woyaoxuexi <154347188@qq.com>
2024-04-19 01:14:17 +0800

    打印一个主页

--- demo-spring/demo-springcloud/security/src/main/java/com/hundsun/demo/springcloud/security/SecurityApplication.java
+++ demo-spring/demo-springcloud/security/src/main/java/com/hundsun/demo/springcloud/security/SecurityApplication.java
@@ -1,8 +1,13 @@
 package com.hundsun.demo.springcloud.security;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import java.awt.*;
+import java.net.URI;
 
 /**
  * @projectName: study-demo
@@ -15,9 +20,21 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan("com.hundsun.demo.springcloud.security.mapper")
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Open the homepage URL in default browser
+        String homepageURL = "http://localhost:9170"; // Update with your homepage URL
+        System.out.println("Unable to open browser automatically. Please navigate to: " + homepageURL);
+        // if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        //     Desktop.getDesktop().browse(URI.create(homepageURL));
+        // } else {
+        //     System.out.println("Unable to open browser automatically. Please navigate to: " + homepageURL);
+        // }
     }
 }
