7352fb1a57bd740c6d8d02ab0e357ccb7f7dd02d
woyaoxuexi <154347188@qq.com>
2024-04-21 22:48:12 +0800

    新增cookie操作

--- helloapp/src/mypack/DispatcherServlet.java
+++ helloapp/src/mypack/DispatcherServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DispatcherServlet extends GenericServlet {
@@ -27,6 +30,14 @@ public class DispatcherServlet extends GenericServlet {
         request.setAttribute("USER", username);
         //在request对象中添加PASSWORD属性
         request.setAttribute("PASSWORD", password);
+
+        Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+        for (Cookie cookie : cookies) {
+            // 在HTTP cookie中，path是一个可选参数，用于指定哪些路径下的页面可以访问该cookie。当服务器发送一个cookie给浏览器时，它可以通过指定path来限制cookie只能被该路径下的页面访问。
+            System.out.println(String.format("name: %s, value: %s, path: %s", cookie.getName(), cookie.getValue(), cookie.getPath()));
+        }
+
+        ((HttpServletResponse)(response)).addCookie(new Cookie("helloapp","123"));
 
         /*把请求转发给hello.jsp */
         ServletContext context = getServletContext();
