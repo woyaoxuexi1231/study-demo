1710f2f6f4fcd1c296cf15340f57a054949e42f2
woyaoxuexi <154347188@qq.com>
2024-04-23 00:38:51 +0800

    新增一个auth-service模块

---
+++ demo-spring/demo-springcloud/auth-service/src/main/java/com/hundsun/demo/auth/service/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.hundsun.demo.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author woaixuexi
+ * @since 2024/4/23 0:36
+ */
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class);
+    }
+}
