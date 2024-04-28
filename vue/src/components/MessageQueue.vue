e7fb21890f5b46ea596f9ba20112708978d880e7
-1+1 <154347188@qq.com>
2024-04-28 20:11:59 +0800

    新增使用redis实现消息的发布/订阅

--- vue/src/components/MessageQueue.vue
+++ vue/src/components/MessageQueue.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <el-button @click="startProgress">点击发送</el-button>
-    <!-- 使用 Element UI 的进度条组件 -->
-    <p></p>
-    <!-- 使用 Element UI 的环形进度条组件 -->
-    <el-progress type="circle" :percentage="progressValue" :status="progressStatus"></el-progress>
-    <p></p>
-    <p></p>
+    <el-container>
+      <el-main>
+        <div>
+          <h1>RabbitMQ</h1>
+          <el-button @click="sendRabbitMsg">点击发送</el-button>
+          <!-- 使用 Element UI 的进度条组件 -->
+          <p></p>
+          <!-- 使用 Element UI 的环形进度条组件 -->
+          <el-progress type="circle" :percentage="rabbitProgressValue" :status="rabbitProgressStatus"></el-progress>
+          <p></p>
+          <p></p>
+        </div>
+      </el-main>
+      <el-main>
+        <div>
+          <h1>Redis</h1>
+          <el-button @click="sendRedisMsg">点击发送</el-button>
+          <!-- 使用 Element UI 的进度条组件 -->
+          <p></p>
+          <!-- 使用 Element UI 的环形进度条组件 -->
+          <el-progress type="circle" :percentage="redisProgressValue" :status="redisProgressStatus"></el-progress>
+          <p></p>
+          <p></p>
+        </div>
+      </el-main>
+    </el-container>
+
   </div>
 </template>
 
@@ -17,17 +37,22 @@ export default {
   data() {
     return {
       // 进度条初始百分比
-      progressValue: 0,
+      rabbitProgressValue: 0,
       // 进度条初始状态，空字符串表示正常状态
-      progressStatus: 'success'
+      rabbitProgressStatus: 'success',
+
+      // 进度条初始百分比
+      redisProgressValue: 0,
+      // 进度条初始状态，空字符串表示正常状态
+      redisProgressStatus: 'success'
     };
   },
   methods: {
     // 发送消息
-    startProgress() {
+    sendRabbitMsg() {
       // 模拟请求发出前的进度
-      this.progressValue = 10;
-      this.progressStatus = 'success';
+      this.rabbitProgressValue = 10;
+      this.rabbitProgressStatus = 'success';
       const url = '/api/rabbimq/sendMsg';
       // 使用 axios 发送 GET 请求
       this.$axios.get(url)
@@ -39,14 +64,42 @@ export default {
             type: 'success'
           });
           // 请求成功，设置百分比为 100%
-          this.progressValue = 100;
+          this.rabbitProgressValue = 100;
         })
         .catch(error => {
           // 请求失败时的处理
           console.error('请求失败:', error);
           // 请求失败，设置百分比为 50%，并显示失败状态
-          this.progressValue = 50;
-          this.progressStatus = 'exception';
+          this.rabbitProgressValue = 50;
+          this.rabbitProgressStatus = 'exception';
+          this.$message.error(url + ': ' + error);
+        });
+    },
+
+    // 发送消息
+    sendRedisMsg() {
+      // 模拟请求发出前的进度
+      this.redisProgressValue = 10;
+      this.redisProgressStatus = 'success';
+      const url = '/api/redispush/publish';
+      // 使用 axios 发送 GET 请求
+      this.$axios.get(url)
+        .then(response => {
+          // 请求成功时的处理
+          console.log('响应数据:', response.data);
+          this.$message({
+            message: '消息发送成功',
+            type: 'success'
+          });
+          // 请求成功，设置百分比为 100%
+          this.redisProgressValue = 100;
+        })
+        .catch(error => {
+          // 请求失败时的处理
+          console.error('请求失败:', error);
+          // 请求失败，设置百分比为 50%，并显示失败状态
+          this.redisProgressValue = 50;
+          this.redisProgressStatus = 'exception';
           this.$message.error(url + ': ' + error);
         });
     },
