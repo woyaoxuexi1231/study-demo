0ace578417ccbd633338e91a59d61872fcc02e8f
-1+1 <154347188@qq.com>
2024-04-29 23:02:42 +0800

    适配 IDEA 2022.1 的自动装配问题

--- demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/idgenerator/SnowFlakeController.java
+++ demo-spring/demo-springboot/src/main/java/com/hundsun/demo/springboot/idgenerator/SnowFlakeController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.CreateMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,7 @@ public class SnowFlakeController {
         System.out.println(snowflakeUtil.snowflakeId(workerId, dataCenterId));
     }
 
+    @Qualifier(value = "curatorFramework")
     @Autowired
     CuratorFramework curatorFramework;
     /**
