de052da0ffb97af15672042601978bf3654b2d9a
hulei <154347188@qq.com>
2024-11-14 09:18:21 +0800

    配置admin直连

--- spring-cloud/admin-server/src/main/java/com/hundsun/demo/spring/boot/admin/AdminServerApplication.java
+++ spring-cloud/admin-server/src/main/java/com/hundsun/demo/spring/boot/admin/AdminServerApplication.java
@@ -3,11 +3,10 @@ package com.hundsun.demo.spring.boot.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class AdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class, args);
