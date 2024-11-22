bb428e450ab5dfba8b1922be4c397284260cd9ae
hulei <154347188@qq.com>
2024-11-23 00:20:56 +0800

    代码更新

---
+++ note/git/搭建git私服.md
@@ -0,0 +1,13 @@
+**Gitea**：一个轻量级的Git服务，支持自建和托管，界面友好，适合小型团队使用。
+
+**Gogs**：类似于Gitea，也是一个轻量级的Git服务，但界面更为简洁，适合个人和小团队。
+
+**GitStack**：一个集成了Git和Bash的Windows服务，方便在Windows环境中使用Git。
+
+**Gogs**：一个轻量级的Git服务，支持自建和托管，界面友好，适合小型团队使用。
+
+**GitLab CE**：GitLab的社区版，完全开源，功能强大，适合大型团队使用。
+
+
+
+[使用源代码安装 | Gitea Documentation](https://docs.gitea.com/zh-cn/installation/install-from-source)
--- spring/spring-boot/src/main/java/org/hulei/springboot/utils/ITextExample.java
+++ spring/spring-boot/src/main/java/org/hulei/springboot/utils/ITextExample.java
@@ -3,6 +3,7 @@ package org.hulei.springboot.utils;
 import com.itextpdf.text.Anchor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BaseFont;
@@ -11,6 +12,8 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileOutputStream;
+import java.net.URL;
+import java.util.Objects;
 
 public class ITextExample {
     public static void main(String[] args) {
@@ -29,31 +32,38 @@ public class ITextExample {
             BaseFont baseFont = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
             BaseFont msYaHeiBaseFont = BaseFont.createFont("经典宋体简.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
             BaseFont harmonyOSBaseFont = BaseFont.createFont("HarmonyOS_Sans_Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-            Font chineseFont = new Font(harmonyOSBaseFont, 12, Font.NORMAL);
+            Font chineseFont = new Font(baseFont, 12, Font.NORMAL);
 
-            // 添加标题
-            document.add(new Paragraph("生成PDF范例", chineseFont));
-            document.add(new Paragraph("使用iText支持中文的PDF生成。", chineseFont));
+            // // 添加标题
+            // document.add(new Paragraph("生成PDF范例", chineseFont));
+            // document.add(new Paragraph("使用iText支持中文的PDF生成。", chineseFont));
+            //
+            // // 添加表格
+            // PdfPTable table = new PdfPTable(3); // 3列表格
+            // table.addCell(new PdfPCell(new Phrase("列1", chineseFont)));
+            // table.addCell(new PdfPCell(new Phrase("列2", chineseFont)));
+            // table.addCell(new PdfPCell(new Phrase("列3", chineseFont)));
+            // for (int i = 1; i <= 9; i++) {
+            //     table.addCell(new PdfPCell(new Phrase("单元格 " + i, chineseFont)));
+            // }
+            // document.add(table);
 
-            // 添加表格
-            PdfPTable table = new PdfPTable(3); // 3列表格
-            table.addCell(new PdfPCell(new Phrase("列1", chineseFont)));
-            table.addCell(new PdfPCell(new Phrase("列2", chineseFont)));
-            table.addCell(new PdfPCell(new Phrase("列3", chineseFont)));
-            for (int i = 1; i <= 9; i++) {
-                table.addCell(new PdfPCell(new Phrase("单元格 " + i, chineseFont)));
-            }
-            document.add(table);
-
-            // 添加图片（可选）
-            // Image image = Image.getInstance("path_to_image.jpg"); // 替换为你的图片路径
-            // image.scaleToFit(200, 200); // 设置图片大小
-            // document.add(image);
-
+            // document.newPage();
             // 添加超链接
             Anchor link = new Anchor("点击打开百度", chineseFont);
             link.setReference("https://www.baidu.com");
             document.add(link);
+
+
+            // 添加图片（可选）
+            // ClassLoader classLoader = ITextExample.class.getClassLoader();
+            // URL logoUrl = classLoader.getResource("OIP.jpg");
+            //
+            // Image image = Image.getInstance(Objects.requireNonNull(logoUrl)); // 替换为你的图片路径
+            // image.scaleToFit(200, 200); // 设置图片大小
+            // document.add(image);
+
+            document.newPage();
 
             // Step 5: 关闭文档
             document.close();
