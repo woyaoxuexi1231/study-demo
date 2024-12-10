a2c425407f2612f8ba56e616ccba0aa472a7a630
hulei <154347188@qq.com>
2024-12-10 22:36:02 +0800

    refactor: pom文件优化，笔记更新，es配置更新

--- script/package-all.cmd
+++ script/package-all.cmd
@@ -1,5 +1,24 @@
+@echo off
+REM 设置错误级别为0
+set ERRORLEVEL=0
 
+REM 定义你的项目路径
+set PROJECT_PATH=C:\project\study-demo
+set MAVEN_PATH=C:\Program Files\JetBrains\IntelliJ IDEA 2024.2.5\plugins\maven\lib\maven3\bin
 
+REM 进入项目目录
+cd /d %PROJECT_PATH%\dependencies-parent
 
-D:\Java\jdk-17.0.12_windows-x64_bin\jdk-17.0.12\bin\java.exe -Dmaven.multiModuleProjectDirectory=D:\project\study-demo\dependencies-parent -Djansi.passthrough=true "-Dmaven.home=E:\IntelliJ IDEA 2024.2.3\plugins\maven\lib\maven3" "-Dclassworlds.conf=E:\IntelliJ IDEA 2024.2.3\plugins\maven\lib\maven3\bin\m2.conf" "-Dmaven.ext.class.path=E:\IntelliJ IDEA 2024.2.3\plugins\maven\lib\maven-event-listener.jar" -Dcool.request.port=63679 "-javaagent:E:\IntelliJ IDEA 2024.2.3\lib\idea_rt.jar=64908:E:\IntelliJ IDEA 2024.2.3\bin" -Dfile.encoding=UTF-8 -classpath "E:\IntelliJ IDEA 2024.2.3\plugins\maven\lib\maven3\boot\plexus-classworlds-2.8.0.jar;E:\IntelliJ IDEA 2024.2.3\plugins\maven\lib\maven3\boot\plexus-classworlds.license;C:\Users\49038\.config\.cool-request\request\lib\spring-invoke-starter.jar" org.codehaus.classworlds.Launcher -Didea.version=2024.2.5 --update-snapshots -s "C:\Users\49038\.m2\settings - 副本.xml" -Dmaven.repo.local=D:\mavenrepository clean
+REM 执行Maven打包命令
+echo maven package...
+%MAVEN_PATH\mvn clean install -DskipTests
 
+REM 检查Maven命令是否成功执行
+if %ERRORLEVEL% neq 0 (
+    echo Maven package failed %ERRORLEVEL%
+) else (
+    echo Maven package successful
+)
+
+REM 暂停以便查看结果
+pause
