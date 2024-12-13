ed0c81d1b7fbf4f48ea3087e15cd1f0556310e9a
hulei <154347188@qq.com>
2024-12-13 10:43:05 +0800

    feat: 新增主页相关配置，路由前缀配置

---
+++ spring/spring-data/spring-data-redis/src/main/java/org/hulei/springboot/redis/config/HelloController.java
@@ -0,0 +1,13 @@
+package org.hulei.springboot.redis.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "forward:index-v2.html";
+    }
+}
